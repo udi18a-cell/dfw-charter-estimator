5689d080e4b1a87d3abae5e5f074f3bf50c43fc9
Udi Anyiam <udi18a@gmail.com>
2026-04-25 11:46:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-{
-  "semi": false,
-  "singleQuote": true,
-  "trailingComma": "all"
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+plugins: [react()],
+})
